c9be257ff8280e67765a3d5bfc62b043447baf6c
Mikhail Ostashchenko <mikeostashenko@gmail.com>
2022-07-04 17:27:36 +0300

    add header file

---
+++ mathAlgo.h
@@ -0,0 +1,20 @@
+#ifndef MATHALGO_H_
+#define MATHALGO_H_
+
+//#include <iostream>
+
+namespace mathAlgo{
+
+	// Abs
+	double iabs(double number){
+		if(number < 0){
+			return -number;
+		}
+		else{
+			return number;
+		}
+	}
+	
+}
+
+#endif
